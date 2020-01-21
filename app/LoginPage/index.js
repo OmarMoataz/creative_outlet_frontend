fe06c6245f681c2afddb5aa7c30701620153b9a6
Omar Moataz <omarmoataz@outlook.com>
2020-01-21 17:49:12 +0200

    Added newly designed login page. Supports mobile.

--- app/LoginPage/index.js
+++ app/LoginPage/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import { withRouter } from 'react-router';
+import { withRouter } from "react-router";
 import * as Yup from "yup";
 import { authenticationService } from "../_services/authenticationService";
 
@@ -16,7 +16,6 @@ class LoginPage extends Component {
   render() {
     return (
       <div>
-        <h2> Login </h2>
         <Formik
           initialValues={{
             email: "",
@@ -30,7 +29,7 @@ class LoginPage extends Component {
             setStatus();
             authenticationService.login(email, password).then(
               user => {
-                this.props.history.push('/');
+                this.props.history.push("/");
               },
               error => {
                 setSubmitting(false);
@@ -40,19 +39,49 @@ class LoginPage extends Component {
           }}
         >
           {({ status, isSubmitting }) => (
-            <Form>
-              <div>
-                <label htmlFor="email">Email</label>
-                <Field name="email" type="text" />
-                <ErrorMessage name="email" component="div" />
+            <Form className="px-4">
+              <h1 className="my-1 font-bold text-lg"> Login </h1>
+              <div className="mb-4">
+                <label
+                  className="mb-1 w-full lg:w-56 text-sm block"
+                  htmlFor="email"
+                >
+                  My Email
+                </label>
+                <Field
+                  placeholder="john.doe@example.com"
+                  className="w-full lg:w-56 lg:mr-1 text-sm p-1 border"
+                  name="email"
+                  type="text"
+                />  
+                <ErrorMessage
+                  className="inline-block text-red-600"
+                  name="email"
+                  component="div"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="mb-1 text-sm block" htmlFor="password">
+                  My Password
+                </label>
+                <Field
+                  placeholder="my password"
+                  className="w-full lg:w-56 lg:mr-1 text-sm p-1 border"
+                  name="password"
+                  type="password"
+                />
+                <ErrorMessage
+                  className="inline-block text-red-600"
+                  name="password"
+                  component="div"
+                />
               </div>
               <div>
-                <label htmlFor="password">Password</label>
-                <Field name="password" type="password" />
-                <ErrorMessage name="password" component="div" />
-              </div>
-              <div>
-                <button type="submit" disabled={isSubmitting}>
+                <button
+                  className="w-full lg:w-56 bg-gray-500 border"
+                  type="submit"
+                  disabled={isSubmitting}
+                >
                   Login
                 </button>
               </div>
