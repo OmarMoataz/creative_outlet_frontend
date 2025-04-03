8b0daa4ac70c09be295d71ccd1aec7018cae92bd
Omar Moataz <omarmoataz@outlook.com>
2025-04-03 02:02:16 +0200

    - Updated prod URL.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,17 +1,20 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
-const path = require('path');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+const path = require("path");
 
-module.exports = merge({
-  mode: 'production',
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "index_bundle.js"
+module.exports = merge(
+  {
+    mode: "production",
+    output: {
+      path: path.resolve(__dirname, "./dist"),
+      filename: "index_bundle.js",
+    },
+    externals: {
+      // global app config object
+      config: JSON.stringify({
+        apiUrl: "https://creative-outlet.fly.dev",
+      }),
+    },
   },
-  externals: {
-    // global app config object
-    config: JSON.stringify({
-      apiUrl: "https://creative-outlet-api.herokuapp.com"
-    })
-  }
-}, common);
+  common
+);
